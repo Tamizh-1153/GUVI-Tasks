9b644e0f3d622e933ac902c6e12fcbc23632f656
Tamizh-1153 <tamizh1153@gmail.com>
2023-03-28 12:29:02 +0530

    Day 3 tasks
    resume and for loops in JSON

---
+++ B46English/Day 3/Q2.Resume JSON.js
@@ -0,0 +1,19 @@
+let resume={
+    fullName:{
+        fname:"Tamil",
+        lname:"Arasan",
+    },
+    education:{
+        sslc:94,
+        hsc:96,
+        college:70
+
+    },
+    skills:{
+        skill1:'C++',
+        skill2:'Javascript',
+        skill3:'HTML',
+        skill4:'CSS'
+    }       
+}
+console.log((resume))
